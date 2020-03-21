c1d2437b35eb4397152028732b7e8f8c316b9d01
Carlos Milán Figueredo <cmilanf@hispamsx.org>
2020-03-21 23:40:59 +0100

    Converted tabs to whitespaces

--- scripts/clean-gpu-aks.bash
+++ scripts/clean-gpu-aks.bash
@@ -1,40 +1,40 @@
 #!/bin/bash
 VERSION='1.0'
 headermsg () {
-	echo "Folding@home Azure Kubernetes Service DELETE script v${VERSION}"
-	echo "Copyright (c) 2020 Carlos Milán Figueredo - MIT License"
-	echo ""
+    echo "Folding@home Azure Kubernetes Service DELETE script v${VERSION}"
+    echo "Copyright (c) 2020 Carlos Milán Figueredo - MIT License"
+    echo ""
 }
 
 usagemsg () {
-	echo "$0 -g <resource group name> --parameters-file <parameters file> --subscription <Azure subscription name or id>"
-	echo "   --iknowwhatiamdoing"
-	echo ""
+    echo "$0 -g <resource group name> --parameters-file <parameters file> --subscription <Azure subscription name or id>"
+    echo "   --iknowwhatiamdoing"
+    echo ""
 }
 while [ $# -gt 0 ]
 do
-	case "$1" in
-		-g)
-			RESOURCE_GROUP_NAME="$2"
-			shift 2
-			;;
-		--parameters-file)
-			PARAMETERS_FILE="$2"
-			shift 2
-			;;
-		--subscription)
-			SUBSCRIPTION="$2"
-			shift 2
-			;;
+    case "$1" in
+        -g)
+            RESOURCE_GROUP_NAME="$2"
+            shift 2
+            ;;
+        --parameters-file)
+            PARAMETERS_FILE="$2"
+            shift 2
+            ;;
+        --subscription)
+            SUBSCRIPTION="$2"
+            shift 2
+            ;;
         --iknowwhatiamdoing)
             IKNOW=true
             shift 1
             ;;
-		*)
-			echo "Incorrect arguments or syntax fail: $1"; echo ''
-			exit 1
-			;;
-	esac
+        *)
+            echo "Incorrect arguments or syntax fail: $1"; echo ''
+            exit 1
+            ;;
+    esac
 done
 if [ "$IKNOW" = true ]; then
     AAD_SP_CLIENTID=$(jq -r .parameters.existingServicePrincipalClientId.value ${PARAMETERS_FILE})
--- scripts/deploy-gpu-aks.bash
+++ scripts/deploy-gpu-aks.bash
@@ -1,22 +1,22 @@
 #!/bin/bash
 VERSION='1.0'
 headermsg () {
-	echo "Folding@home Azure Kubernetes Service deployment script v${VERSION}"
-	echo "...with GPU support via CUDA!"
-	echo "Copyright (c) 2020 Carlos Milán Figueredo - MIT License"
-	echo ""
+    echo "Folding@home Azure Kubernetes Service deployment script v${VERSION}"
+    echo "...with GPU support via CUDA!"
+    echo "Copyright (c) 2020 Carlos Milán Figueredo - MIT License"
+    echo ""
 }
 
 usagemsg () {
-	echo "$0 -g <resource group name> --template-file <template file> --parameters-file <parameters file>"
-	echo "		--subscription <Azure subscription name or id> [--create-aad-sp] [--location <Azure region>]"
-	echo ""
+    echo "$0 -g <resource group name> --template-file <template file> --parameters-file <parameters file>"
+    echo "		--subscription <Azure subscription name or id> [--create-aad-sp] [--location <Azure region>]"
+    echo ""
 }
 
 chkbin () {
     if ! [ -x "$(command -v $1)" ]; then
-	    echo "ERROR: This script requires $1 program to continue. Please install it."
-	    exit 1
+        echo "ERROR: This script requires $1 program to continue. Please install it."
+        exit 1
     fi
 }
 
@@ -25,8 +25,8 @@ chkbin () {
 # $2 -> Variable with the parameter value
 # $3 -> Parameter file name
 jqsubs () {
-	jq ".parameters.$1.value = \$argument" --arg argument "$2" $3 \
-    	> $3.tmp && mv -f $3.tmp $3
+    jq ".parameters.$1.value = \$argument" --arg argument "$2" $3 \
+        > $3.tmp && mv -f $3.tmp $3
 }
 
 headermsg
@@ -35,19 +35,19 @@ AKS_CLUSTER_NAME=''
 DNS_PREFIX=''
 while [ $# -gt 0 ]
 do
-	case "$1" in
-		-g)
-			RESOURCE_GROUP_NAME="$2"
-			shift 2
-			;;
-		--template-file)
-			TEMPLATE_FILE="$2"
-			shift 2
-			;;
-		--parameters-file)
-			PARAMETERS_FILE="$2"
-			shift 2
-			;;
+    case "$1" in
+        -g)
+            RESOURCE_GROUP_NAME="$2"
+            shift 2
+            ;;
+        --template-file)
+            TEMPLATE_FILE="$2"
+            shift 2
+            ;;
+        --parameters-file)
+            PARAMETERS_FILE="$2"
+            shift 2
+            ;;
         --subscription)
             SUBSCRIPTION="$2"
             shift 2
@@ -56,30 +56,30 @@ do
             CREATE_AAD_SP='yes'
             shift 1
             ;;
-		--location)
-			LOCATION="$2"
-			shift 2
-			;;
-		--aks-cluster-name)
-			AKS_CLUSTER_NAME="$2"
-			shift 2
-			;;
-		--dns-prefix)
-			DNS_PREFIX="$2"
-			shift 2
-			;;
-		*)
-			echo "ERROR: Incorrect arguments or syntax fail: $1"; echo ''
-			usagemsg
-			exit 1
-			;;
-	esac
+        --location)
+            LOCATION="$2"
+            shift 2
+            ;;
+        --aks-cluster-name)
+            AKS_CLUSTER_NAME="$2"
+            shift 2
+            ;;
+        --dns-prefix)
+            DNS_PREFIX="$2"
+            shift 2
+            ;;
+        *)
+            echo "ERROR: Incorrect arguments or syntax fail: $1"; echo ''
+            usagemsg
+            exit 1
+            ;;
+    esac
 done
 
 if [ -z "${RESOURCE_GROUP_NAME}" ] || [ -z "${TEMPLATE_FILE}" ] || [ -z "${PARAMETERS_FILE}" ] \
-	|| [ -z "${SUBSCRIPTION}" ]; then
-	echo "ERROR: required parameter is missing."
-	exit 2
+    || [ -z "${SUBSCRIPTION}" ]; then
+    echo "ERROR: required parameter is missing."
+    exit 2
 fi
 
 chkbin jq
@@ -92,9 +92,9 @@ cp -f ${PARAMETERS_FILE} ${OUTPUT_PARAMETERS_FILE}
 
 DATE=$(date '+%Y%m%d%H%M%S')
 if ! [ -z ${AKS_CLUSTER_NAME} ]; then
-	jqsubs 'resourceName' ${AKS_CLUSTER_NAME} ${OUTPUT_PARAMETERS_FILE}
+    jqsubs 'resourceName' ${AKS_CLUSTER_NAME} ${OUTPUT_PARAMETERS_FILE}
 else
-	AKS_CLUSTER_NAME=$(jq -r .parameters.resourceName.value ${OUTPUT_PARAMETERS_FILE})
+    AKS_CLUSTER_NAME=$(jq -r .parameters.resourceName.value ${OUTPUT_PARAMETERS_FILE})
 fi
 if [ "${CREATE_AAD_SP}" == 'yes' ]; then
     echo "Creating Service Principal..."
@@ -104,10 +104,10 @@ if [ "${CREATE_AAD_SP}" == 'yes' ]; then
     SP_APPID=$(echo ${SP} | jq -r .appId)
     SP_PWD=$(echo ${SP} | jq -r .password)
     SP_OBJECTID=$(az ad sp show --id ${SP_NAME} --query objectId -o tsv)
-	jqsubs 'existingServicePrincipalClientId' "${SP_APPID}" ${OUTPUT_PARAMETERS_FILE}
-	jqsubs 'existingServicePrincipalClientSecret' "${SP_PWD}" ${OUTPUT_PARAMETERS_FILE}
-	jqsubs 'existingServicePrincipalObjectId' "${SP_OBJECTID}" ${OUTPUT_PARAMETERS_FILE}
-	jqsubs 'existingVirtualNetworkResourceGroup' "${RESOURCE_GROUP_NAME}" ${OUTPUT_PARAMETERS_FILE}
+    jqsubs 'existingServicePrincipalClientId' "${SP_APPID}" ${OUTPUT_PARAMETERS_FILE}
+    jqsubs 'existingServicePrincipalClientSecret' "${SP_PWD}" ${OUTPUT_PARAMETERS_FILE}
+    jqsubs 'existingServicePrincipalObjectId' "${SP_OBJECTID}" ${OUTPUT_PARAMETERS_FILE}
+    jqsubs 'existingVirtualNetworkResourceGroup' "${RESOURCE_GROUP_NAME}" ${OUTPUT_PARAMETERS_FILE}
 fi
 
 echo "Generating GNU/Linux root SSH key pair..."
@@ -116,23 +116,23 @@ jqsubs 'linuxAdminSshPublicKey' "$(cat output/linuxAdminSshKey.pub)" ${OUTPUT_PA
 
 if [ "$(jq .parameters.createVnet.value arm/deploy-gpu-aks.parameters.json)" = 'true' ]
 then
-	jqsubs 'existingVirtualNetworkResourceGroup' "${RESOURCE_GROUP_NAME}" ${OUTPUT_PARAMETERS_FILE}
-	jqsubs 'newOrExistingVirtualNetworkName' "${AKS_CLUSTER_NAME}" ${OUTPUT_PARAMETERS_FILE}
+    jqsubs 'existingVirtualNetworkResourceGroup' "${RESOURCE_GROUP_NAME}" ${OUTPUT_PARAMETERS_FILE}
+    jqsubs 'newOrExistingVirtualNetworkName' "${AKS_CLUSTER_NAME}" ${OUTPUT_PARAMETERS_FILE}
 fi
 if ! [ -z ${DNS_PREFIX} ]; then
-	jqsubs 'dnsPrefix' "${DNS_PREFIX}" ${OUTPUT_PARAMETERS_FILE}
+    jqsubs 'dnsPrefix' "${DNS_PREFIX}" ${OUTPUT_PARAMETERS_FILE}
 fi
 sed -i '/^$/d' ${OUTPUT_PARAMETERS_FILE}
 
 echo "Deploying managed Kubernetes cluster..."
 if [ $(az group exists -g ${RESOURCE_GROUP_NAME} --subscription ${SUBSCRIPTION}) = 'false' ]; then
-	az group create -n ${RESOURCE_GROUP_NAME} -l ${LOCATION} --subscription ${SUBSCRIPTION}
+    az group create -n ${RESOURCE_GROUP_NAME} -l ${LOCATION} --subscription ${SUBSCRIPTION}
 fi
 az deployment group create \
-	-g ${RESOURCE_GROUP_NAME} \
-	-n ${AKS_CLUSTER_NAME}-deployment \
-	--template-file ${TEMPLATE_FILE} \
-	--parameters @${OUTPUT_PARAMETERS_FILE} \
+    -g ${RESOURCE_GROUP_NAME} \
+    -n ${AKS_CLUSTER_NAME}-deployment \
+    --template-file ${TEMPLATE_FILE} \
+    --parameters @${OUTPUT_PARAMETERS_FILE} \
     --subscription ${SUBSCRIPTION} \
-	--verbose
+    --verbose
 echo "FINISHED: You can find Azure AD SP credentials and root SSH keys at output folder."
